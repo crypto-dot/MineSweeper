22a6c7df77f7e13072dfaed1ab360ea8747d89fe
Carlos Arbizu <carlos2345532@gmail.com>
2022-07-11 23:30:26 -0500

    finished mine map class and cell class

---
+++ src/minesweeper/Cell.java
@@ -0,0 +1,10 @@
+package minesweeper;
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JButton;
+
+public class Cell {
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+}
